6a16c926ea9be05243964b754af550d7574b0d3f
The Dumb <info@hemisphera.com>
2025-02-19 20:51:03 +0100

    Add ZipExtensions

---
+++ Hsp.Extensions.Io.Test/ZipExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.IO.Compression;
+using Bogus;
+
+namespace Hsp.Extensions.Io.Test;
+
+public class ZipExtensions
+{
+  private readonly Faker _faker = new();
+
+  [Fact]
+  public void CreateZipFileFromString()
+  {
+    var content = _faker.Lorem.Paragraphs(2);
+    var filename = _faker.System.FileName();
+
+    using var ms = new MemoryStream();
+    using (var zf1 = new ZipArchive(ms, ZipArchiveMode.Create, true))
+    {
+      zf1.AddEntry(filename, content);
+    }
+
+    ms.Position = 0;
+    using (var zf2 = new ZipArchive(ms, ZipArchiveMode.Read))
+    {
+      Assert.Single(zf2.Entries);
+      using (var sr = new StreamReader(zf2.GetEntry(filename).Open()))
+      {
+        var actualContent = sr.ReadToEnd();
+        Assert.Equal(content, actualContent);
+      }
+    }
+  }
+}
---
+++ Hsp.Extensions.Io/ZipExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace Hsp.Extensions.Io
+{
+  /// <summary>
+  /// Extensions for ZIP archives
+  /// </summary>
+  public static class ZipExtensions
+  {
+    /// <summary>
+    /// Adds an entry with the given name and the text contents (and encoding) to the ZIP archive.
+    /// </summary>
+    /// <param name="archive">The ZIP archive.</param>
+    /// <param name="entryName">The name of the archive.</param>
+    /// <param name="content">The content to add.</param>
+    /// <param name="encoding">The encoding to use for the contents.</param>
+    public static void AddEntry(this ZipArchive archive, string entryName, string content, Encoding? encoding = null)
+    {
+      using (var ms = new MemoryStream())
+      using (var sw = new StreamWriter(ms, encoding ?? Encoding.UTF8, 4096, true))
+      {
+        sw.Write(content);
+        sw.Flush();
+        ms.Position = 0;
+        archive.AddEntry(entryName, ms);
+      }
+    }
+
+    /// <summary>
+    /// Adds an entry with the given name and the data contents to the ZIP archive.
+    /// </summary>
+    /// <param name="archive">The ZIP archive.</param>
+    /// <param name="entryName">The name of the archive.</param>
+    /// <param name="content">The content to add.</param>
+    public static void AddEntry(this ZipArchive archive, string entryName, byte[] content)
+    {
+      using (var ms = new MemoryStream(content))
+      {
+        archive.AddEntry(entryName, ms);
+      }
+    }
+
+    /// <summary>
+    /// Adds an entry with the given name and the data contents to the ZIP archive.
+    /// </summary>
+    /// <param name="archive">The ZIP archive.</param>
+    /// <param name="entryName">The name of the archive.</param>
+    /// <param name="content">The content to add.</param>
+    public static void AddEntry(this ZipArchive archive, string entryName, Stream content)
+    {
+      var entry = archive.CreateEntry(entryName);
+      using (var s = entry.Open())
+      {
+        content.CopyTo(s);
+      }
+    }
+  }
+}
